42ccbf81d89aae5393ce2e0feae2e957ead567be
Angel Soto <airsoto@gmail.com>
2026-09-02 15:55:09 +0200

    Fix mini player Chrome build

--- lib/src/screens.dart
+++ lib/src/screens.dart
@@ -139,9 +139,9 @@ class MiniPlayer extends StatelessWidget {
         IconButton(tooltip: 'Volver al inicio', icon: const Icon(Icons.restart_alt), onPressed: a.restart),
         IconButton(tooltip: 'Atrás 5 segundos', icon: const Icon(Icons.replay_5), onPressed: () => a.skipBy(-5)),
         IconButton(tooltip: a.playing ? 'Pausar' : 'Reproducir', icon: Icon(a.playing ? Icons.pause_circle_filled : Icons.play_circle_fill), iconSize: 34, onPressed: a.toggle),
-        IconButton(tooltip: 'Adelante 20 segundos', icon: const Icon(Icons.forward_20), onPressed: () => a.skipBy(20)),
+        IconButton(tooltip: 'Adelante 20 segundos', icon: const Icon(Icons.fast_forward), onPressed: () => a.skipBy(20)),
         IconButton(tooltip: 'Siguiente episodio o parte', icon: const Icon(Icons.skip_next), onPressed: a.next),
-      ])]),
+      ])),
     ])));
   }
 }
